e305e32327c368cb3d73cc1d008b711f745d4b6b
Camilo <dev1@e-advising.biz>
2023-09-07 15:19:48 -0500

    Fin de seccion 7

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from '../../../../03-gifs-app/src/app/home-page/home-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
